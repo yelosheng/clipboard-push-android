bfccfdcf1b34ba1171659c56c9432a0f1e9dbba9
Huang Sheng <huangsheng8964@gmail.com>
2026-02-28 22:06:32 +0800

    chore: bump version to 1.1.0 (code 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 7
-        versionName = "1.0.9"
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
