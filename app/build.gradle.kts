aef5a1643c1c473f47213977506fa1f8fe9f1c8b
Huang Sheng <huangsheng8964@gmail.com>
2026-03-08 09:12:28 +0800

    chore: bump version to 1.1.9 (code 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.clipboardpush.plus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "1.1.8"
+        versionCode = 10
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
