9421a981552aca7a207ba0de1f452728abf7f416
Huang Sheng <huangsheng8964@gmail.com>
2026-03-11 20:21:48 +0800

    chore: bump version to 1.1.12 (code 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.clipboardpush.plus"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.1.11"
+        versionCode = 11
+        versionName = "1.1.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
